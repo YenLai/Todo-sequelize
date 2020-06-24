e6718f61824e98c7dd00ba5f9224361b3e4629f0
Laisam <b540915@gmail.com>
2020-06-24 18:32:37 +0800

    feat: add basic function

--- app.js
+++ app.js
@@ -2,19 +2,36 @@ const express = require('express')
 const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
+const routes = require('./routes')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 
 const app = express()
 const PORT = 3000
 
+
 app.engine('hbs', hbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.use(session({
+  secret: 'MySecretKey',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(flash())
+usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.use(methodOverride('method'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
---
+++ routes/modules/users.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+
+
+const db = require('../../models')
+const User = db.User
+
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword)
+    errors.push({ message: '每個欄位都是必填。' })
+  if (confirmPassword !== password)
+    errors.push({ message: '密碼與確認密碼不相符。' })
+
+  User.findOne({ where: { email } }).then(user => {
+    if (user)
+      errors.push({ message: '該 Email 已經被註冊過。' })
+    if (errors.length)
+      return res.render('register', { errors, name, email, password, confirmPassword })
+
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name, email, password: hash
+      }))
+      .then(() => {
+        req.flash('success_msg', '帳號註冊成功')
+        res.redirect('/users/login')
+      })
+      .catch(error => console.log(error))
+  })
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '你已經成功登出。')
+  res.redirect('/users/login')
+})
+
+module.exports = router
